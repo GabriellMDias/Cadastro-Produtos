dc04cc77d505a7bd9a8022c75a738dbe7068dd5c
Gabriel Dias <gabrielmoreiradias270402@gmail.com>
2023-04-18 10:07:38 -0300

    Update dbScript

--- dbScript.sh
+++ dbScript.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-sudo -u postgres psql -c "CREATE ROLE userdb WITH LOGIN PASSWORD '123456' SUPERUSER CREATEDB CREATEROLE REPLICATION BYPASSRLS;"
+read -p "1 - Criar Banco de Dados"$'\n'"2 - Excluir Banco de dados"$'\n' option
 
-sudo -u postgres psql -c "CREATE DATABASE userdb;"
+if [ $option -eq 1 ]
+        then
+        sudo -u postgres psql -c "CREATE ROLE userdb WITH LOGIN PASSWORD '123456' SUPERUSER CREATEDB CREATEROLE REPLICATION BYPASSRLS;"
+        sudo -u postgres psql -c "CREATE DATABASE userdb;"
+        sudo -u postgres psql -d userdb -c "CREATE TABLE produto (
+                id_produto serial PRIMARY KEY,
+                descricao varchar(50) NOT NULL,
+                stock int DEFAULT 0,
+                categoria varchar(50) NOT NULL
+                );"
+elif [ $option -eq 2 ]
+        then
+        echo "Excluir banco de dados..."
+	sudo -u postgres psql -c "DROP DATABASE userdb;"
+	sudo -u postgres psql -c "DROP ROLE userdb;"
 
-sudo -u postgres psql -d userdb -c "CREATE TABLE produto (
-id_produto serial PRIMARY KEY,
-descricao varchar(50) NOT NULL,
-stock int DEFAULT 0,
-categoria varchar(50) NOT NULL
-);"
+else
+    echo "Opção Inválida!"
+fi
